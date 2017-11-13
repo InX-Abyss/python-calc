51d2e22e5147f349e5426ae5632ef327f67fdf75
AtroposOrbis <wesley.reid@inxabyss.com>
2017-11-13 18:23:09 -0500

    First commit :D

---
+++ Calcumulator.py
@@ -0,0 +1,14 @@
+print("This is a calculator")
+Type = input("Would you like +, -, *, or /?")
+
+if Type is "+":
+    print('You chose addition!')
+    first = input("Please type the first number")
+    second = input("Please type the second number")
+    print(int(first) + int(second))
+if Type is "-":
+    print('You chose subtraction')
+if Type is "*":
+    print('You chose multiplication')
+if Type is "/":
+    print('You chose division')
