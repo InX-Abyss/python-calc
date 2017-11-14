7e4e21f6beff823ab61af4d38f4da7c167bcc3f2
AtroposOrbis <wesley.reid@inxabyss.com>
2017-11-13 21:45:29 -0500

    v1 Update

--- Better Calculator.py
+++ Better Calculator.py
@@ -1,16 +1,23 @@
 print("This is a calculator")
 Type = input("Would you like '+', '-', '*', or '/'?")
 
-if Type is "+":
-    try:
-        number = int(input("Enter your first value."))
-    except ValueError:
-        print("please enter an integer")
 
-    if number == "0"
-        pass
-    numbers = []
-    numbers.append(number)
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a valid number!")
+
+
+numbers = []
+
+if Type is "+":
+    num = 1
+    while num >= 0:
+        num = get_int("Enter your values (One at a time). Press enter to get the sum: ")
+        numbers.append(num)
+
 
 if Type is "-":
     pass
