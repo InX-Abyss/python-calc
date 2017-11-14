b04634224e7087997fafdbcb0fe4a14b1e19e4b0
anthony <anthony.rainy@hotmail.com>
2017-11-13 23:22:15 -0500

    Working Addition v.05

--- Better Calculator.py
+++ Better Calculator.py
@@ -1,16 +1,18 @@
+from functools import reduce
 print("This is a calculator")
 Type = input("Would you like '+', '-', '*', or '/'?")
 addition = []
+subtraction = []
 if Type == "+":
     number = input("Enter your first value.")           #String value
     while True:
-        if number =="-":
-                addition.pop
         try:
             number = int(number)                        #try to convert to integer
             addition.append(number)
             print(addition)
         except ValueError:                              #If it fails to convert
+            if number == '-':
+                addition.pop(-1)
             number = input("please enter an integer")
 
 
@@ -20,13 +22,26 @@ if Type == "+":
             if number == "":
                 print(sum(addition))
 
-#     if number == "0":
-#         pass
-#     numbers = []
-#     numbers.append(number)
-#
-# if Type is "-":
-#     pass
+
+if Type is "-":
+    number = input("Enter your first value.")  # String value  #Currently Not Functioning
+    while True:
+        try:
+            number = -int(number)  # try to convert to integer
+            subtraction.append(number)
+            print(subtraction)
+        except ValueError:  # If it fails to convert
+            if number == '-':
+                subtraction.pop(-1)
+            number = input("please enter an integer")
+
+
+
+        else:
+            number = input("Enter another value, if none press enter")
+            if number == "":
+                print(sum(addition))
+
 #
 # if Type is "*":
 #     pass
