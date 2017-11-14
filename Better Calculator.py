1b93c73724ade35b32951857bd99210ae074cea4
AtroposOrbis <wesley.reid@inxabyss.com>
2017-11-13 19:31:37 -0500

    Beautify code

--- Better Calculator.py
+++ Better Calculator.py
@@ -4,12 +4,19 @@ Type = input("Would you like '+', '-', '*', or '/'?")
 if Type is "+":
     pass
 
-    if Type is "-":
-        pass
+if Type is "-":
+    pass
 
-    if Type is "*":
-        pass
+if Type is "*":
+    pass
+
+if Type is "-":
+    pass
+
+if Type is "*":
+    pass
 
 if Type is "/":
+    pass
 
-    '''Added functionality for all basic operations'''
+'''Added functionality for all basic operations'''
