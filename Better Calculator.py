080ec3c624917904bd2c49eb877367197c366c96
anthony <anthony.rainy@hotmail.com>
2017-11-13 19:28:03 -0500

    Added functionality for all basic math operations

--- Better Calculator.py
+++ Better Calculator.py
@@ -2,9 +2,14 @@ print("This is a calculator")
 Type = input("Would you like '+', '-', '*', or '/'?")
 
 if Type is "+":
-    Values = input('How many numbers are a part of this equation')
-if Type is "-":
-if Type is "*":
+    pass
+
+    if Type is "-":
+        pass
+
+    if Type is "*":
+        pass
+
 if Type is "/":
 
     '''Added functionality for all basic operations'''
