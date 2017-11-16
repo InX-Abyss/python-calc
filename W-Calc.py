90015f00f47f77c319bb580dd66be0e669a484ab
AtroposOrbis <wesley.reid@inxabyss.com>
2017-11-15 22:40:29 -0500

    New version of W-Calc | v2

--- W-Calc.py
+++ W-Calc.py
@@ -4,35 +4,56 @@ from functools import reduce
 def get_int(prompt):
     while True:
         try:
-            return int(input(prompt))
+            n1 = input(prompt)
+            if n1 == "":
+                return "finish"
+            if n1 == "finish":
+                return "finish"
+            return int(n1)
+
         except ValueError:
             print("Please enter a valid number!")
 
 
 def get_nums():
     while True:
-        num = get_int(f"\nEnter your values one at a time.\nEnter '0' to finish.\n>>>>> ")
-        if num == 0:
+        num = get_int(f"\nEnter your values one at a time.\nPress enter without a value to finish.\n>>>>> ")
+        if num == "finish":
             break
         yield num
 
 
 operations = {
-    '+': lambda x, y: x + y,
-    '-': lambda x, y: x - y,
-    '*': lambda x, y: x * y,
-    '/': lambda x, y: x / y,
-    '%': lambda x, y: x % y
+    '+': lambda x, y: x + y,  # Lambda is simply a placeholder for the function name.
+    '-': lambda x, y: x - y,  # Since this is a dictionary, we can call these functions by their keys.
+    '*': lambda x, y: x * y,  # The Keys are the beginning section of each line
+    '/': lambda x, y: x / y,  # <function key>: <function name and arguments go here>: <function goes here>
+    '%': lambda x, y: x % y   # lambda creates anonymous functions. (Functions not bound to a name.)
 }
 
-print("This is a calculator")
+# MORE LAMBDA EXPLANATION
+# def f(x): return x**2
+# For example we could quickly create a variable based function by typing: <name> = lambda x: x**2
 
 while True:
-    oper = input(f'Enter the operation symbol you wish to perform,\nOperations: [{", ".join(operations.keys())}]\n>>> ')
-    if oper in operations:
-        break
-    else:
-        print('Pick a proper operator!')
+    print("This is a calculator")
+    while True:
+        oper = input(f'Enter the operation symbol you wish to perform,\nOperations: [{", ".join(operations.keys())}]\n>>> ')
+        if oper in operations:
+            break
+        else:
+            print('Pick a proper operator!')
 
-print('Enter all the values you wish to process:')
-print(f'\nThe result of applying {oper} to the values is:\n{reduce(operations[oper], get_nums())}\n\nGoodbye!')
+    print('Enter all the values you wish to process:\n')
+    while True:
+        try:
+            result = reduce(operations[oper], get_nums())
+            break
+        except TypeError:
+            print('At least enter one value to compute a result!')
+
+    print(f'The result of applying {oper} to the values is:\n{result}\n')
+
+    again = input('Type "exit" to terminate, or press enter to start again.')
+    if again == "exit":
+        break
